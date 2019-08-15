281284d7e03fb9bd26f3e1b43aa54ec06fa7fb72
YangDR <ydrml@hotmail.com>
2019-08-15 10:43:44 +0800

    增加文档

--- drivers/src/main/kotlin/serialport/parser/SerialPortFinder.kt
+++ drivers/src/main/kotlin/serialport/parser/SerialPortFinder.kt
@@ -2,19 +2,47 @@ package serialport.parser
 
 import com.fazecast.jSerialComm.SerialPort
 
+/**
+ * 通用串口查找器（DSL）
+ */
 class SerialPortFinder<T> private constructor() {
+    /**
+     * 波特率
+     */
     var baudRate: Int = 9600
+
+    /**
+     * 临时缓存容量
+     */
     var bufferSize: Int = 256
+
+    /**
+     * 查找超时时间
+     */
     var timeoutMs: Int = 1000
 
+    /**
+     * 发送激活码
+     */
     var activate: ByteArray = byteArrayOf()
-    private var predicate: (T) -> Boolean = { false }
 
+    /**
+     * 设置成功条件
+     */
     fun condition(block: (T) -> Boolean) {
         predicate = block
     }
 
+    // 判断谓词
+    private var predicate: (T) -> Boolean = { false }
+
     companion object {
+        /**
+         * DSL
+         *
+         * @param engine 解析引擎
+         * @param block  DSL
+         */
         fun <T> findSerialPort(
             engine: ParseEngine<Byte, T>,
             block: SerialPortFinder<T>.() -> Unit) =
@@ -24,19 +52,19 @@ class SerialPortFinder<T> private constructor() {
                     SerialPort
                         .getCommPorts()
                         .find { port ->
+                            // 设置串口
                             port.baudRate = baudRate
                             port.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 100, 100)
                             if (!port.openPort()) return@find false
-
+                            // 发送激活码
                             if (activate.isNotEmpty()
                                 && activate.size != port.writeBytes(activate, activate.size.toLong())
                             ) return@find false
-
+                            // 初始化接收
                             val time = System.currentTimeMillis()
-
                             val array = ByteArray(bufferSize)
                             var result = false
-
+                            // 接收并解析
                             while (!result && System.currentTimeMillis() - time < timeoutMs) {
                                 val actual = port.readBytes(array, array.size.toLong()).also(::println)
                                 if (actual <= 0) continue
@@ -44,7 +72,7 @@ class SerialPortFinder<T> private constructor() {
                                     result = result || predicate(temp)
                                 }
                             }
-
+                            // 返回
                             result
                         }
                 }
--- drivers/src/test/kotlin/TestFindPort.kt
+++ drivers/src/test/kotlin/TestFindPort.kt
@@ -1,11 +1,12 @@
 import serialport.parser.SerialPortFinder.Companion.findSerialPort
 
-object MarvelMindTest {
+object MarvelmindTest {
     @JvmStatic
     fun Array<String>.main() {
         findSerialPort(com.marvelmind.engine()) {
             baudRate = 115200
             timeoutMs = 1000
+            bufferSize = 32
             condition { (code, _) -> code == 0x11 }
         }
             ?.descriptivePortName
@@ -13,12 +14,13 @@ object MarvelMindTest {
     }
 }
 
-object FaseLaseTest {
+object FaselaseTest {
     @JvmStatic
     fun Array<String>.main() {
-        findSerialPort(com.faselase.engine(true)) {
+        findSerialPort(com.faselase.engine(filter = true)) {
             baudRate = 460800
             timeoutMs = 5000
+            bufferSize = 32
             activate = "#SF 10\r\n".toByteArray(Charsets.US_ASCII)
             condition { (rho, _) -> rho > 0 }
         }
