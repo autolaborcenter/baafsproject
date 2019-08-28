7d7042e577d11b9ec8b0d8ac95e042f745bc9d16
YangDR <ydrml@hotmail.com>
2019-08-28 16:55:48 +0800

    调整依赖关系

--- pathfollower/build.gradle.kts
+++ pathfollower/build.gradle.kts
@@ -8,10 +8,4 @@ dependencies {
     implementation(files("../libs/common-collection-0.1.0-3.jar"))
     implementation(files("../libs/common-collection-0.1.0-3-sources.jar"))
     implementation(project(":transform"))
-
-    implementation(project(":locator"))
-    testImplementation(project(":drivers"))
-    testImplementation("net.java.dev.jna", "jna", "+")
-    testImplementation(files("../libs/pm1sdk-v0.0.1.jar"))
-    testImplementation(files("../libs-test/consoleparser-0.1.9.jar"))
 }
