5c3e359d21089406e7f8938b9d8a662b706459ad
gsc07 <370441373@qq.com>
2019-10-25 18:18:51 +0800

    新增障碍物显示测试

--- library/src/test/java/cn/autolabor/baafs/DrawLidarTask.java
+++ library/src/test/java/cn/autolabor/baafs/DrawLidarTask.java
@@ -7,7 +7,10 @@ import cn.autolabor.core.annotation.TaskProperties;
 import cn.autolabor.core.server.ServerManager;
 import cn.autolabor.core.server.executor.AbstractTask;
 import cn.autolabor.core.server.message.MessageHandle;
+import cn.autolabor.message.navigation.Msg2DOdometry;
 import cn.autolabor.message.navigation.Msg2DPoint;
+import cn.autolabor.message.navigation.Msg2DPose;
+import cn.autolabor.message.navigation.Msg2DTwist;
 import cn.autolabor.plugin.gazebo.ignitionmsgs.MarkerProtos;
 import cn.autolabor.plugin.gazebo.msg.MsgMarker;
 import cn.autolabor.plugin.gazebo.task.GazeboMarkerServerTask;
@@ -20,13 +23,65 @@ public class DrawLidarTask extends AbstractTask {
     @InjectMessage(topic = "marker")
     private MessageHandle<byte[]> markerHandle;
 
+    private List<List<Double>> outline;
+    private double predictionTime = 1.0;
+
     public DrawLidarTask(String... name) {
         super(name);
+        outline = (List<List<Double>>) ServerManager.me().getConfig("PoseDetectionTask", "outline");
+        asyncRunLater("carForShow", 1000L);
     }
 
     public static void main(String[] args) {
+        ServerManager.me().loadConfig("conf/obstacle.conf");
         ServerManager.me().register(new GazeboMarkerServerTask());
         ServerManager.me().register(new DrawLidarTask());
+
+    }
+
+    @TaskFunction
+    public void carForShow() {
+        MsgMarker marker = new MsgMarker();
+        marker.setNamespace("default");
+        marker.setId(1L);
+        marker.setMaterial("Gazebo/White");
+        marker.setAction(MarkerProtos.Marker.Action.ADD_MODIFY);
+        marker.setType(MarkerProtos.Marker.Type.LINE_STRIP);
+        outline.forEach(i -> {
+            marker.addPoint(i.get(0), i.get(1), 0.05);
+        });
+        markerHandle.pushSubData(marker.toProtoByte());
+        System.out.println("push car");
+    }
+
+    @SubscribeMessage(topic = "cmdvel_in")
+    @TaskFunction
+    public void moveCarForShow(Msg2DOdometry msg) {
+        Msg2DTwist in = msg.getTwist();
+        Msg2DPose predictionPose = new Msg2DPose();
+        if (in.getYaw() != 0) {
+            double d = in.getX() / in.getYaw();
+            double theta = in.getYaw() * predictionTime;
+            predictionPose.setX(d * Math.sin(theta));
+            predictionPose.setY(d * (1 - Math.cos(theta)));
+            predictionPose.setYaw(theta);
+        } else {
+            predictionPose.setX(in.getX() * predictionTime);
+        }
+
+        MsgMarker marker = new MsgMarker();
+        marker.setNamespace("default");
+        marker.setId(2L);
+        marker.setMaterial("Gazebo/Red");
+        marker.setAction(MarkerProtos.Marker.Action.ADD_MODIFY);
+        marker.setType(MarkerProtos.Marker.Type.LINE_STRIP);
+        outline.forEach(i -> {
+            double x = i.get(0);
+            double y = i.get(1);
+            marker.addPoint(predictionPose.getX() + x * Math.cos(predictionPose.getYaw()) - y * Math.sin(predictionPose.getYaw()), predictionPose.getY() + x * Math.sin(predictionPose.getYaw()) + y * Math.cos(predictionPose.getYaw()), 0.06);
+        });
+        markerHandle.pushSubData(marker.toProtoByte());
+        System.out.println("push prediction car");
     }
 
     @SubscribeMessage(topic = "obstacle_points")
