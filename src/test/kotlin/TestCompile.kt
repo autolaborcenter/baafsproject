4c823470ecbb6964519d48263c3be9ddb1f275c7
Ydr <ydrml@hotmail.com>
2019-10-27 10:44:58 +0800

    现在单元测试会检查默认脚本是否有编译错误

---
+++ src/test/kotlin/TestCompile.kt
@@ -0,0 +1,23 @@
+import AutolaborScript.ScriptConfiguration
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import java.io.File
+import kotlin.script.experimental.api.ScriptDiagnostic.Severity.ERROR
+import kotlin.script.experimental.api.onFailure
+import kotlin.script.experimental.host.toScriptSource
+import kotlin.script.experimental.jvmhost.BasicJvmScriptingHost
+import kotlin.test.fail
+
+class TestCompile {
+    @Test
+    fun test() {
+        // 查找脚本文件
+        val file = File("default.autolabor.kts")
+        assert(file.exists())
+        runBlocking {
+            BasicJvmScriptingHost().compiler(file.toScriptSource(), ScriptConfiguration)
+        }.onFailure { result ->
+            fail("failed to compile with message: \n${result.reports.filter { it.severity == ERROR }.joinToString("/n")}")
+        }
+    }
+}
