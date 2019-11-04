1a2f7f1b7b3bfbed5f31d22b523329dbdcd508a0
YangDR <ydrml@hotmail.com>
2019-11-04 16:42:58 +0800

    修改仿真测试结构

--- library/src/test/kotlin/org/mechdancer/lidar/LidarSimulation.kt
+++ library/src/test/kotlin/org/mechdancer/lidar/LidarSimulation.kt
@@ -1,18 +1,17 @@
-package cn.autolabor.baafs
+package org.mechdancer.lidar
 
+import cn.autolabor.baafs.outlineFilter
+import cn.autolabor.baafs.robotOutline
 import kotlinx.coroutines.*
 import org.mechdancer.*
 import org.mechdancer.algebra.implement.vector.to2D
-import org.mechdancer.algebra.implement.vector.vector2DOf
 import org.mechdancer.common.Odometry
 import org.mechdancer.common.Stamped
 import org.mechdancer.common.Velocity
 import org.mechdancer.common.Velocity.NonOmnidirectional
 import org.mechdancer.common.shape.Circle
-import org.mechdancer.common.shape.Polygon
 import org.mechdancer.common.toTransformation
 import org.mechdancer.device.LidarSet
-import org.mechdancer.device.PolarFrameCollectorQueue
 import org.mechdancer.geometry.angle.toDegree
 import org.mechdancer.geometry.angle.toRad
 import org.mechdancer.remote.modules.multicast.multicastListener
@@ -20,40 +19,25 @@ import org.mechdancer.remote.presets.remoteHub
 import org.mechdancer.remote.protocol.SimpleInputStream
 import org.mechdancer.simulation.Chassis
 import org.mechdancer.simulation.Lidar
-import org.mechdancer.simulation.random.Normal
 import org.mechdancer.simulation.speedSimulation
 import java.io.DataInputStream
 import java.util.concurrent.atomic.AtomicReference
 import kotlin.concurrent.thread
 import kotlin.math.PI
 
-private fun Polygon.transform(pose: Odometry): Polygon {
-    val tf = pose.toTransformation()
-    return Polygon(vertex.map { tf(it).to2D() })
-}
+private val obstacles =
+    List(10) { i ->
+        listOf(Circle(.14, 32).sample().transform(Odometry.pose(i * .3, +.5)),
+               Circle(.14, 32).sample().transform(Odometry.pose(i * .3, -.5)))
+    }.flatten()
 
-private class SimulationLidar(
-    private val lidar: Lidar,
-    private val onRobot: Odometry,
-    private val cover: List<Polygon>
-) {
-    private val queue = PolarFrameCollectorQueue()
-
-    val toRobot = onRobot.toTransformation()
-    val frame get() = queue.get()
-
-    fun update(t: Long, robotOnMap: Odometry, obstacles: List<Polygon>) {
-        lidar
-            .update(t * 1E-3, robotOnMap, onRobot, cover, obstacles)
-            .map { it.data }
-            .forEach {
-                if (it.distance.isNaN())
-                    queue.refresh(it.angle)
-                else
-                    queue += Stamped(t, it.copy(distance = it.distance * Normal.next(expect = 1.0, sigma = 1E-2)))
-            }
-    }
-}
+private fun simulationLidar(onRobot: Odometry) =
+    SimulationLidar(
+            lidar = Lidar(.15..8.0, 3600.toDegree(), 2E-4)
+                .apply { initialize(.0, Odometry.pose(), 0.toRad()) },
+            onRobot = onRobot,
+            cover = cover,
+            errorSigma = 1E-2)
 
 @ExperimentalCoroutinesApi
 fun main() = runBlocking(Dispatchers.Default) {
@@ -75,44 +59,13 @@ fun main() = runBlocking(Dispatchers.Default) {
         thread(isDaemon = true) { while (true) invoke() }
     }
 
-    val obstacles =
-        List(10) { i ->
-            listOf(
-                Circle(.14, 32).sample().transform(Odometry.pose(i * .3, +.5)),
-                Circle(.14, 32).sample().transform(Odometry.pose(i * .3, -.5)))
-        }.flatten()
-    val cover =
-        listOf(
-            Circle(.07).sample().transform(Odometry.pose(-.36)),
-            Polygon(
-                listOf(
-                    vector2DOf(-.10, +.26),
-                    vector2DOf(-.10, +.20),
-                    vector2DOf(-.05, +.20),
-                    vector2DOf(-.05, -.20),
-                    vector2DOf(-.10, -.20),
-                    vector2DOf(-.10, -.26),
-                    vector2DOf(+.10, -.26),
-                    vector2DOf(+.10, -.20),
-                    vector2DOf(+.05, -.20),
-                    vector2DOf(+.05, +.20),
-                    vector2DOf(+.10, +.20),
-                    vector2DOf(+.10, +.26))))
-
     val chassis = Chassis(Stamped(0L, Odometry.pose()))
-    val front = SimulationLidar(
-        Lidar(.15..8.0, 3600.toDegree(), 2E-4).apply {
-            initialize(.0, Odometry.pose(), 0.toRad())
-        }, Odometry.pose(x = +.113), cover)
-    val back = SimulationLidar(
-        Lidar(.15..8.0, 3600.toDegree(), 2E-4).apply {
-            initialize(.0, Odometry.pose(), 0.toRad())
-        }, Odometry.pose(x = -.138), cover)
-    val lidarSet = LidarSet(
-        mapOf(
-            front::frame to front.toRobot,
-            back::frame to back.toRobot)
-    ) { it !in outlineFilter }
+    val front = simulationLidar(Odometry.pose(x = +.113))
+    val back = simulationLidar(Odometry.pose(x = -.138))
+    val lidarSet =
+        LidarSet(mapOf(front::frame to front.toRobot,
+                       back::frame to back.toRobot)
+        ) { it !in outlineFilter }
 
     val buffer = AtomicReference<NonOmnidirectional>(Velocity.velocity(0, 0))
     launch {
@@ -125,18 +78,23 @@ fun main() = runBlocking(Dispatchers.Default) {
             delay(5000L)
         }
     }
+    // 激光雷达采样
     val lidarSamplePeriod = 50L
     var lidarSampleCount = 0
+    // 运行仿真
     for ((t, v) in speedSimulation { buffer.get() }) {
+        // 控制机器人行驶
         val (_, robotOnMap) = chassis.drive(v)
+        // 更新激光雷达
         front.update(t, robotOnMap, obstacles)
         back.update(t, robotOnMap, obstacles)
-
+        // 绘制机器人外轮廓和遮挡物
         cover.map { it.transform(robotOnMap) }
             .forEachIndexed { i, polygon ->
                 remote.paint("机器人遮挡$i", polygon)
             }
         remote.paint("机器人", robotOutline.transform(robotOnMap))
+        // 激光雷达采样
         if (t > lidarSampleCount * lidarSamplePeriod) {
             ++lidarSampleCount
             val robotToMap = robotOnMap.toTransformation()
