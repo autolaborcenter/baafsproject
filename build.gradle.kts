7d24b3e7683be0364ce8c2270fadd42702270281
Ydr <ydrml@hotmail.com>
2020-02-09 17:32:53 +0800

    改线性代数库的 bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ allprojects {
 subprojects {
     dependencies {
         // 子项目自动依赖重要数学和定义库
-        implementation(files("../libs/linearalgebra-0.2.7-dev-2.jar"))
+        implementation(files("../libs/linearalgebra-0.2.7-dev-3.jar"))
         implementation(files("../libs/simulator-0.0.3.jar"))
     }
 }
